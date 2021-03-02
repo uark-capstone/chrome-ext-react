41a42783241f0f371ae60532113e6e56e5af494d
RyanRau <ryanzrau@gmail.com>
2021-03-01 23:05:39 -0600

    adjusted build info and lecture selection

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -7,11 +7,14 @@ const SignIn = () => {
   let DESTINATION_URL = process.env.REACT_APP_DESTINATION_URL;
   let BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
   
+  const [userID, setUserID] = useState(-1);
+  const [lectureID, setLectureID] = useState(-1);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [showBuildInfo, setShowBuild] = useState(false);
 
   const [email, setEmail] = useState("af027@uark.edu");
   const [password, setPassword] = useState("password");
-  const [isLoading, setIsLoading] = useState(false);
   const [result, setResult] = useState("");
 
   const requestOptions = {
@@ -40,15 +43,18 @@ const SignIn = () => {
       })
       .then(responseData => {
         if(responseData != null){
-          let destinationURL = DESTINATION_URL + responseData.id + '/' + '1';
- 
-          chrome.tabs.create({ url: destinationURL, active: false, pinned: true });
-          window.close();
-        }
-        setIsLoading(false);
+          setUserID(responseData.id)
+          setIsLoggedIn(true);
+        }        
       })
       .catch((error) => console.error(error));
   };
+
+  const joinLecture = (e) => {
+    let destinationURL = DESTINATION_URL + userID + '/' + lectureID;
+    chrome.tabs.create({ url: destinationURL, active: false, pinned: true });
+    window.close();
+  }
 
   return (
     <div>
@@ -61,44 +67,78 @@ const SignIn = () => {
         />
         </div>
         
-        {/* <h1 style={{ position: "absolute", left: "200px" }}> Team 10!!</h1> */}
-        <Form style={{ padding: "50px", padding: "150px", paddingBottom:"120px"}}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <div style={{ padding: "50px", padding: "150px", paddingBottom:"120px"}}>
+          {isLoggedIn
+          ? <Form>
+              <Form.Group>
+                <Form.Label>Lecture ID</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter Lecture Id"
+                    value={lectureID}
+                    onChange={(e) => setLectureID(e.target.value)}
+                  />
+              </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+              <Button
+                style={{ position: "absolute", left: "200px" }}
+                variant="primary"
+                type="submit"
+                onClick={(e) => joinLecture(e)}
+              >
+                Join Lecture
+              </Button>
+            </Form>
+          : <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-          <Button
-            style={{ position: "absolute", left: "200px" }}
-            variant="primary"
-            type="submit"
-            onClick={(e) => login(e)}
-          >
-            Submit
-          </Button>
-        </Form>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+
+              <Button
+                style={{ position: "absolute", left: "200px" }}
+                variant="primary"
+                type="submit"
+                onClick={(e) => login(e)}
+              >
+                Submit
+              </Button>
+            </Form>
+          }
+        </div>
+        
+      {result}  
+      
+      <p
+        onClick={() => setShowBuild(!showBuildInfo)}>
+        Build Information
+      </p>
+      {showBuildInfo &&
+        <div>
+          <p>Destination URL: {DESTINATION_URL}</p> 
+          <p>Backend URL: {BACKEND_URL}</p> 
+        </div>
+      }
+    
       </div>
-      {result}
-      {DESTINATION_URL}
-      {BACKEND_URL}
     </div>
   );
 };
